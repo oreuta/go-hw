4deea4186ee2ddc26ad0eb3e75f9835f3144da9d
Oleksandr Reuta <reutaov@gmail.com>
2021-01-03 21:22:29 +0200

    Update main.go
    some suggestions

--- lesson-3/letter-swap/main.go
+++ lesson-3/letter-swap/main.go
@@ -8,9 +8,11 @@ import (
 	"strings"
 )
 
-const validationRegexp = "[^a-zA-Z0-9 .,!?-()]"
+var validationRegexp = regexp.MustCompile("[^a-zA-Z0-9 .,!?-()]") // this is a heavy operation. let's do it only once
 
-var encodeVowels = map[rune]rune{
+type vowelMapping map[rune]rune // we use it more than one time, so let's define it as a custom type
+
+var encodeVowels = vowelMapping{
 	'a': '1',
 	'e': '2',
 	'i': '3',
@@ -23,7 +25,7 @@ var encodeVowels = map[rune]rune{
 	'U': '5',
 }
 
-var decodeVowels = map[rune]rune{
+var decodeVowels = vowelMapping{
 	'1': 'a',
 	'2': 'e',
 	'3': 'i',
@@ -31,12 +33,17 @@ var decodeVowels = map[rune]rune{
 	'5': 'u',
 }
 
+var mapping = map[string]vowelMapping{ // it's a static data, so using a predefined mapping we can avoid switch (additional complexity in run-rime)
+	"encode" : encodeVowels,
+	"decode" : decodeVowels,
+}
+
 func main() {
 	var mode string
 	fmt.Print("Do you want to decode string or encode it? [encode/decode]: ")
 	_, err := fmt.Scanln(&mode)
 
-	if err != nil || (mode != "encode" && mode != "decode") {
+	if err != nil || (mode != "encode" && mode != "decode") { // it'd be better to separate technical and logical issues (err and data checks)
 		fmt.Print("There are two options only. Please, enter a valid one.")
 		return
 	}
@@ -47,36 +54,31 @@ func main() {
 
 	for scanner.Scan() {
 		input = scanner.Text()
-		break
+		break // if you want to read only one line, just don't use for-loop
 	}
 
 	input = strings.Trim(input, " ")
 
-	if err != nil || !validInput(input) {
+	if !validInput(input) {
 		fmt.Print("Error. Please, make sure you use latin characters, numbers or punctuation characters only.")
 		return
 	}
 
-	switch mode {
-	case "encode":
-		fmt.Println("Your result: ", swapString(input, encodeVowels))
-	case "decode":
-		fmt.Println("Your result: ", swapString(input, decodeVowels))
-	}
-
+	fmt.Println("Your result: ", swapString(input, mapping[mode]))
 }
 
 func validInput(s string) bool {
-	match, _ := regexp.MatchString(validationRegexp, s)
-
-	return !match && len(s) > 0
+	if s == "" { // to avoid making a heavy regexp operation if it is not needed
+		return false
+	}
+ 	return validationRegexp.MatchString(s)
 }
 
-func swapString(s string, mapping map[rune]rune) string {
+func swapString(s string, mapping vowelMapping) string {
 	return strings.Map(func(r rune) rune {
 		value, ok := mapping[r]
 
-		if ok == false {
+		if !ok {
 			return r
 		}
 
